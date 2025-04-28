4ca92ddbff5fa94802e83c123ddd13919019ef8d
deenharo01 <decentnharo01@gmail.com>
2025-04-28 22:02:12 +0200

    Update config.cjs

--- config.cjs
+++ config.cjs
@@ -3,7 +3,7 @@ const fs = require("fs");
 require("dotenv").config();
 
 const config = {
-  SESSION_ID: process.env.SESSION_ID || "nector~I9oUgKyJ#kOuae8ng6czmJ-SETB4PvcaOIzi5pTNwiH4xn73idLA",
+  SESSION_ID: process.env.SESSION_ID || "nector~J0ojCZiB#5m-v6ztZM6hezIpTg3qjIdUQz_ocfHcYDNYsMErj7Tw",
   PREFIX: process.env.PREFIX || '*',
   AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN !== undefined ? process.env.AUTO_STATUS_SEEN === 'true' : true, 
   AUTO_STATUS_REPLY: process.env.AUTO_STATUS_REPLY !== undefined ? process.env.AUTO_STATUS_REPLY === 'true' : true,
